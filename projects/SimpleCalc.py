41ddf87a4f79556d96c1fa538554c78b8a59fdf0
YoussefAlbarbari <134265353+YoussefAlbarbari@users.noreply.github.com>
2023-06-22 11:48:39 +0300

    Update SimpleCalc.py

--- projects/SimpleCalc.py
+++ projects/SimpleCalc.py
@@ -1,10 +1,13 @@
 def calculator():
+    # Function to add two numbers
     def add(a, b):
         return a + b
 
+    # Function to subtract two numbers
     def subtract(a, b):
         return a - b
 
+    # Function to divide two numbers, handles division by zero error
     def divide(a, b):
         try:
             return a / b
@@ -12,12 +15,15 @@ def calculator():
             print("Error: Division by zero.")
             return None
 
+    # Function to multiply two numbers
     def multiply(a, b):
         return a * b
 
+    # Function to calculate the percentage of a value
     def calculate_percentage(value, percentage):
         return value * (percentage / 100)
 
+    # Function to calculate the percentage change between two values
     def calculate_percentage_change(old_value, new_value):
         try:
             return ((new_value - old_value) / old_value) * 100
@@ -25,29 +31,33 @@ def calculator():
             print("Error: Division by zero.")
             return None
 
+    # Function to clear the result (currently not used)
     def clear_result():
         result_label = ""
 
+    # Function to perform the calculation based on user input
     def calculate():
-        operations = operation_var.get()
-        num1 = num1_entry.get()
-        num2 = num2_entry.get()
+        operations = operation_var.get()  # Get the selected operation from user input
+        num1 = num1_entry.get()  # Get the first number from user input
+        num2 = num2_entry.get()  # Get the second number from user input
 
         if not num1.isdigit() or not num2.isdigit():
+            # Check if the input numbers are valid (numeric)
             print("Error: Invalid input. Numbers must be numeric.")
             return
 
-        num1 = float(num1)
-        num2 = float(num2)
-        result = ""
+        num1 = float(num1)  # Convert the first number to a float
+        num2 = float(num2)  # Convert the second number to a float
+        result = ""  # Initialize the result variable as an empty string
 
+        # Perform the corresponding operation based on user input
         if operations == "add":
             result = add(num1, num2)
         elif operations == "sub" or operations == "subtract":
             result = subtract(num1, num2)
         elif operations == "div" or operations == "divide":
             result = divide(num1, num2)
-        elif operations == "mul"or operations == "multiply":
+        elif operations == "mul" or operations == "multiply":
             result = multiply(num1, num2)
         elif operations == "percentage":
             result = calculate_percentage(num1, num2)
@@ -58,16 +68,18 @@ def calculator():
             return
 
         if result is not None:
-            print("Result:", result)
+            print("Result:", result)  # Print the calculated result
 
+    # Welcome message and available operations
     print("Welcome to the simple calculator")
     print("add   subtract   divide   multiply   percentage   percentage_change")
 
+    # User input for numbers and operation
     num1_entry = input("Enter the first number: ")
     num2_entry = input("Enter the second number: ")
     operation_var = input("Choose the operation: ")
 
-    calculate()
+    calculate()  # Call the calculate() function to perform the calculation
 
 # Call the calculator function to start the program
 calculator()
